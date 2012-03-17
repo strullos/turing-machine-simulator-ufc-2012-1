ee444db817ede786bc9aed375b39f255b18397d5
vitor.shade@gmail.com <vitor.shade@gmail.com@7e9d0f7c-b843-deb4-2890-b008f9f88617>
2012-03-17 20:46:08 +0000

    -Implementadas funções básicas da Máquina de Turing -Criado arquivo main -Realizados alguns testes unitários da classe Máquina no arquivo main.

--- trunk/src/maquina/maquina.cpp
+++ trunk/src/maquina/maquina.cpp
@@ -2,16 +2,92 @@
  * maquina.cpp
  *
  *  Created on: 17/03/2012
- *      Author: felipe
+ *      Author: Vitor
  */
 
 #include "maquina.h"
 
-Maquina::Maquina() {
-	// TODO Auto-generated constructor stub
+#include <iostream>
 
+Maquina::Maquina(const std::string& fita, unsigned int tamanho_da_fita)
+{
+	//Inicializa a fita como definida na nossa convenção (@, #, etc)
+	m_fita.append("@#" + fita);
+	m_fita.resize(tamanho_da_fita,'#');
+	m_tamanho_da_fita = tamanho_da_fita;
+
+	// Poe a cabeça de leitura na posição inicial, no caso: 2
+	m_pos_atual = 2;
 }
 
-Maquina::~Maquina() {
-	// TODO Auto-generated destructor stub
+//Move a cabeça de leitura para a esquerda e retorna 'true' caso não haja problemas
+bool Maquina::mover_esquerda()
+{
+	if(m_pos_atual > 0){
+		m_pos_atual--;
+		return true;
+	}
+	return false;
 }
+
+//Move a cabeça de leitura para a direita e retorna 'true' caso não haja problemas
+bool Maquina::mover_direita()
+{
+	//Para uma fita de tamanho n, temos que a posição da cabeça de leitura
+	//varia de 0 a n-1, por isso essa verificação
+	if(m_pos_atual < m_tamanho_da_fita - 1){
+		m_pos_atual++;
+		return true;
+	}
+	return false;
+}
+
+
+bool Maquina::escrever(char simbolo[1])
+{
+//	std::string::iterator it1;
+//	std::string::iterator it2;
+//	it1 = m_fita.begin();
+//	it2 = m_fita.begin();
+//	std::advance(it1,m_pos_atual);
+//	std::advance(it2,m_pos_atual+1);
+//	m_fita.replace(it1,it2,simbolo.c_str(),1);
+
+	//Substitui o simbolo na posição atual da cabeça de leitura
+	m_fita.replace(m_pos_atual,1,simbolo);
+	return true;
+}
+
+
+char Maquina::simbolo_atual()
+{
+	std::string::iterator it;
+	it = m_fita.begin();
+	//Avança o iterador da string para o
+	//caractere na posiçao atual da cabeça de leitura
+	std::advance(it,m_pos_atual);
+	return (*it);
+}
+
+
+const std::string& Maquina::fita()
+{
+	return m_fita;
+}
+
+void Maquina::print_tape(){
+	std::string::iterator it;
+	unsigned int curr_pos = 0;
+	for(it = m_fita.begin(); it != m_fita.end(); it++){
+		if(curr_pos == m_pos_atual){
+			std::cout << "_";
+		}
+		std::cout << (*it);
+		if(curr_pos != m_pos_atual - 1){
+			std::cout << " ";
+		}
+		curr_pos++;
+	}
+	std::cout << std::endl;
+}
+
--- trunk/src/maquina/maquina.h
+++ trunk/src/maquina/maquina.h
@@ -2,16 +2,33 @@
  * maquina.h
  *
  *  Created on: 17/03/2012
- *      Author: felipe
+ *      Author: Vitor
  */
 
 #ifndef MAQUINA_H_
 #define MAQUINA_H_
 
+#include <string>
+
+/*
+
+
+ */
 class Maquina {
 public:
-	Maquina();
-	virtual ~Maquina();
+	Maquina(const std::string& fita, unsigned int tamanho_da_fita); //< Recebe uma string representando a fita e o tamanho total da fita
+	virtual ~Maquina() {}; //< Destrutor
+	bool mover_esquerda(); //< Move a cabeça de leitura 1 posição para a esquerda / Decrementa m_pos_atual
+	bool mover_direita();  //< Move a cabeça de leitura 1 posição para a direita  / Incrementa m_pos_atual
+	bool escrever(char simbolo[1]); //< Escreve "simbolo" na fita na posição atual
+	char simbolo_atual(); //< Retorna o símbolo da fita na posição atual
+	const std::string& fita(); //< Retorna a fita
+
+	void print_tape(); // remove later
+private:
+	unsigned int m_pos_atual; //< Representa a cabeça de leitura da máquina / a posição atual na fita
+	unsigned int m_tamanho_da_fita; //< Tamanho máximo da fita
+	std::string m_fita; //< O conteúdo da fita
 };
 
 #endif /* MAQUINA_H_ */
