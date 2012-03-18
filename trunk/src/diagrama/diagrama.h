dd3b49f8200c13fec7b985c0bc097fb5a8726dd6
vitor.shade@gmail.com <vitor.shade@gmail.com@7e9d0f7c-b843-deb4-2890-b008f9f88617>
2012-03-18 00:35:21 +0000

    Criados métodos para carregar módulos e carregar ações de um diagrama. Corrigido bug no destrutor de Diagrama.

--- trunk/src/diagrama/diagrama.h
+++ trunk/src/diagrama/diagrama.h
@@ -13,6 +13,12 @@
 
 #include "../modulo/modulo.h"
 
+//Representa uma ação do diagrama
+class AcDiagrama{
+public:
+	std::map<std::string, std::string> m_ac;
+};
+
 class Diagrama {
 public:
 	Diagrama();
@@ -20,7 +26,11 @@ public:
 	virtual ~Diagrama();
 private:
 	bool carregar_modulo(std::string& linha_modulo);
-	bool configurar_acoes(std::string& linha_acao);
+	bool carregar_acoes(std::string& linha_acao);
+	bool remover_espacos_brancos(std::string& linha);
+	bool remover_valor_da_linha(std::string& linha);
+	bool pegar_remover_valor_da_linha(std::string&linha, std::string& valor);
+	std::multimap<std::string, AcDiagrama> m_acoes_diagrama; //< Hash indexado pelo nome do módulo, contendo as ações que ele pode realizar.
 	std::map<std::string,Modulo*> m_modulos_carregados; //< Hash indexado pelo nome do arquivo do módulo. Cada módulo só pode ser carregado uma única vez
 	std::map<std::string,Modulo*> m_modulos; //Hash indexado pelo nome de um módulo. Um módulo carregado pode ter vários nomes (referências)
 };
