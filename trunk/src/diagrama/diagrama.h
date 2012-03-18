8c26d37da9a0236f08f30f4a3613bdad353750b1
vitor.shade@gmail.com <vitor.shade@gmail.com@7e9d0f7c-b843-deb4-2890-b008f9f88617>
2012-03-18 14:53:24 +0000

    Comentários adicionados na classe Diagrama Criado método para resetar o Diagrama

--- trunk/src/diagrama/diagrama.h
+++ trunk/src/diagrama/diagrama.h
@@ -14,60 +14,86 @@
 
 #include "../modulo/modulo.h"
 
-//Representa uma ação do diagrama
-class Regra{
+/*!
+ * Representa uma regra do diagrama.
+ * Cada regra é referente apenas a um único módulo e é composta por um conjunto de
+ * símbolos de leitura e Módulos associados.
+ * Uma regra do diagrama especifica que, especifica o próximo módulo a ser executado, de
+ * acordo com o símbolo atual na cabeça de leitura.
+ *
+ * Obs: Para evitar a criação de mais arquivos, essa classe foi definida toda inline.
+ */
+class RegraDiagrama{
 public:
-	Regra(std::string &simbolo, std::string &modulo_final){
+	RegraDiagrama(std::string &simbolo, std::string &modulo_final){
 		m_qualquer_simbolo = false;
 		inserir(simbolo,modulo_final);
-	}
-	Regra(){ m_qualquer_simbolo = false; };
+	}													 //!< Construtor
+	RegraDiagrama(){ m_qualquer_simbolo = false; };		 //!< Destrutor
 
 	void inserir(std::string simbolo, std::string modulo_final){
-		m_acoes.insert(std::pair<std::string,std::string>(simbolo,modulo_final));
-	}
+		m_regras.insert(std::pair<std::string,std::string>(simbolo,modulo_final));
+	}													//!< Insere uma nova regra
 
 	std::string pegar_prox_modulo(std::string simbolo_atual){
 		std::map<std::string,std::string>::iterator it;
-		if(m_acoes.size() > 0){
+		if(m_regras.size() > 0){
+			//Se não importar o símbolo, retorna o primeiro módulo do hash
 			if(m_qualquer_simbolo){
 				std::map<std::string,std::string>::iterator it;
-				it = m_acoes.begin();
+				it = m_regras.begin();
 				return (*it).second;
 			}else{
-				if(m_acoes.find(simbolo_atual) != m_acoes.end()){
-					return m_acoes[simbolo_atual];
+				//Se não, retorna o módulo aproximado
+				if(m_regras.find(simbolo_atual) != m_regras.end()){
+					return m_regras[simbolo_atual];
 				}
 			}
-			return "";
-		}else{
-			return "";
 		}
-
-	}
-	std::map<std::string, std::string> m_acoes;
-	bool m_qualquer_simbolo;
+		return "";
+	}													//!< Retorna o próximo módulo a ser executado
+	std::map<std::string, std::string> m_regras;		//!< Hash, contendo o símbolo esperado na cabeça de leitura e o nome do próximo módulo
+	bool m_qualquer_simbolo;							//!< Determina se essa regra é do tipo que aceita qualquer símbolo na cabeça de leitura
 };
 
+/*!
+ * Representa um diagrama. É composta por um conjunto de módulos e regras.
+ * Responsável pelo carregamento e inicialização dos módulos e das regras.
+ * Controla também a execução de um diagrama.
+ *
+ * Esta classe contém 3 containers principais:"
+ * -m_modulos_carregados: Um mapa indexado pelo nome do módulo, tal que cada entrada armazena um ponteiro para um módulo.
+ * -m_modulos: Um mapa indexado pelo nome do módulo, tal que cada entrada também armazena um ponteiro para um módulo.
+ * -m_regras: Um mapa indexado pelo nome do módulo, tal que cada entrada armazena um objeto Regras que contém as regras
+ * relativas a cada módulo.
+ *
+ * Cada módulo só é carregado uma única vez, mas um módulo pode ter várias referências no diagrama.
+ * Exemplo: R1 r.mt , R2 r.mt, etc...
+ * Antes de um novo módulo ser adicionado na tabela, é verificado se o módulo já foi carregado. Caso tenha
+ * sido, apenas uma nova referência é adicionada à tabela.
+ *
+ *
+ */
 class Diagrama {
 public:
-	Diagrama();
-	bool carregar_diagrama(std::string arquivo);
-	virtual ~Diagrama();
-	void print_diagram(); //remove later
+	Diagrama(); 										//!< Construtor
+	virtual ~Diagrama(); 								//!< Destrutor; Limpa todas as estruturas de dados
 
-	void executar(std::string fita_inicial, unsigned int tamanho_da_fita);
+	bool carregar_diagrama(std::string arquivo); 		//!< Carrega um diagrama especificad por 'arquivo'
+	void print_diagram(); 								//!< Imprime o diagrama atualmente carregado
+	void executar(std::string fita_inicial,
+			unsigned int tamanho_da_fita); 				//!< Executa o diagrama carregado.
 private:
-	bool carregar_modulo(std::string& linha_modulo);
-	bool carregar_regras(std::string& linha_acao);
+	bool carregar_modulo(std::string& linha_modulo); 	//!< Carrega um módulo especicado num arquivo de diagrama
+	bool carregar_regra(std::string& linha_regra); 		//!< Carrega uma regra especicada num arquivo de diagrama
+	void limpar();										//!< Reseta os valores do diagrama
 
-	std::map<std::string, Regra*> m_regras; //< Hash indexado pelo nome do módulo, contendo as ações que ele pode realizar.
-	std::map<std::string,Modulo*> m_modulos_carregados; //< Hash indexado pelo nome do arquivo do módulo. Cada módulo só pode ser carregado uma única vez
-	std::map<std::string,Modulo*> m_modulos; //Hash indexado pelo nome de um módulo. Um módulo carregado pode ter vários nomes (referências)
+	std::map<std::string, RegraDiagrama*> m_regras_modulos; 	//!< Hash indexado pelo nome do módulo, contendo as ações que ele pode realizar.
+	std::map<std::string,Modulo*> m_modulos_carregados; //!< Hash dos módulos carregados, indexado pelo nome do arquivo do módulo.
+	std::map<std::string,Modulo*> m_modulos; 			//!< Hash dos módulos e suas referências indexado pelo nome de um módulo.
 
-	std::string m_modulo_inicial;
-
-	bool m_carregado;
+	std::string m_modulo_inicial; 						//!< Armazena o nome do módulo inicial
+	bool m_carregado;									//!< Determina se o diagrama especificado foi carregado com sucesso
 };
 
 #endif /* DIAGRAMA_H_ */
