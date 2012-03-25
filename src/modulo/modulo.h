b1e3e3f8abc1da6d3cd18b0a3ac9530567291c86
Ffmmjj <Ffmmjj@gmail.com>
2012-03-25 16:01:40 +0000

    Checagem para não-determinismo adicionada; Iniciada implementação para suporte a parametros nos modulos.

--- src/modulo/modulo.h
+++ src/modulo/modulo.h
@@ -11,28 +11,34 @@
 #include <map>
 #include "../maquina/maquina.h"
 
+typedef struct {
+	char simbolo;
+	std::string estado2;
+	char acao;
+}Regra, *Regrap;
+
 class Modulo {
 public:
 	Modulo(const std::string &arquivo);
 	virtual ~Modulo();
 
 	bool inicializar();
-	bool executar(Maquina *m);
+	bool executar(Maquina *m, char var_value = '\0');
 	unsigned int linha_incorreta();
-private:
-	typedef struct {
-		char simbolo;
-		std::string estado2;
-		char acao;
-	}Regra;
 
+private:
 	std::string m_estado_inicial;
 	std::string m_arquivo;
 	std::multimap<std::string, Regra> m_regras;
 	unsigned int m_linha_incorreta;
 	bool m_inicializado;
+	char m_var, m_var_value;
 
-	bool aplica_regra(Maquina *m, const Regra &r);
+	bool aplica_regra(Maquina *m, const Regrap r);
+	const Regrap procura_regra(std::string estado, char simbolo);
+	bool processa_cabecalho(std::string linha);
+	bool processa_variavel(std::string linha);
+	bool processa_regra(std::string linha);
 };
 
 #endif /* MODULO_H_ */
