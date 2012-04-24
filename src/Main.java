89881b3333314fb1769a66d3ab5abe9e2ddfa856
vitor.shade <vitor.shade@gmail.com>
2012-04-24 04:13:13 +0000

    -Main modificado para receber os parâmetros.

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,15 @@ import teoria.simulador.modulo.ModuloFactory;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
-		IModulo raiz = null;
-		Maquina fita = null;
-		Diagrama d = new Diagrama();
-		d.carregar("char_copy.dt");
-		d.imprime_diagrama();
-		d.executar("aabb");
-//		
-//		if( args.length == 2) {
-//			raiz = ModuloFactory.construir(args[0]);
-//			fita = new Fita(args[2]);
-//		} else {
-//			System.out.println("Para rodar um diagrama, passe o nome do arquivo por parametro e o estado inicial da fita.");
-//			System.out.println("O modulo inicial pode ser especificado com um '%' antes do nome do modulo.");
-//			System.out.println("Caso nenhum modulo inicial seja especificado, o primeiro modulo a ser carregado sera o inicial.");
-//			return ;
-//		}
-//		
-//		if( raiz == null ) {
-//			System.out.println("Falha ao carregar maquina!");
-//			return ;
-//		}
-//		
-//		while( raiz.executar_passo(fita) ) {
-//			System.out.println(raiz.estado_atual());
-//		}
+		if( args.length == 2) {			
+			Diagrama d = new Diagrama();
+			d.carregar(args[0]);			
+			d.executar(args[1]);
+		} else {
+			System.out.println("Para rodar um diagrama, passe o nome do arquivo por parametro e o estado inicial da fita.");
+			System.out.println("O modulo inicial pode ser especificado com um '%' antes do nome do modulo.");
+			System.out.println("Caso nenhum modulo inicial seja especificado, o primeiro modulo a ser carregado sera o inicial.");
+			return ;
+		}		
 	}
 }
