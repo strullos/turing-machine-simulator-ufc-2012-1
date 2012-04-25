681727e805932f205053c06b0b54872c8a7f0a4b
vitor.shade@gmail.com <vitor.shade@gmail.com@7e9d0f7c-b843-deb4-2890-b008f9f88617>
2012-04-25 02:06:56 +0000

    -Branch quebrado deletado.

--- branches/TuringMachineSimulator_JavaWeb/src/teoria/simulador/modulo/DescritorRegra.java
+++
@@ -1,9 +0,0 @@
-package teoria.simulador.modulo;
-
-public class DescritorRegra{
-	public boolean m_atualiza_var;
-	public boolean m_envia_var;
-	String m_variavel_atualizada;
-	String m_variavel_enviada;
-	String m_prox_modulo;
-}
