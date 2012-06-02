320dcd81fab402e10a2c2292c0e5c04ab81c483c
vitor.shade <vitor.shade@gmail.com>
2012-06-02 20:53:47 +0000

    -Finished Machine implementation -Added methods to print execution steps -Modified Module to be an abstract class instead of an interface

--- src/Module.java
+++ src/Module.java
@@ -2,14 +2,21 @@ import java.io.FileNotFoundException;
 import java.util.Vector;
 
 
-public interface Module {
+public abstract class Module {
 	public abstract boolean loadFromFile(String filename) throws FileNotFoundException;
-	public abstract boolean loadFromString(Vector<String> content);
+	public abstract boolean load(Vector<String> content);
+	public abstract boolean loadFromString(String content);
+	public abstract void printStep(Tape t);
+	public abstract void printSummary(Tape t);
+	
 	public abstract boolean executeStep(Tape t);
+	public abstract boolean execute(Tape t);
 	public abstract String getCurrentState();	
 	public abstract boolean processRule(String line);
 	public abstract boolean processHeader(String line);
 	
-	public final Log m_log = new Log();
-	public int m_current_line = 1;
+	protected final Log m_log = new Log();
+	protected int m_current_line = 0;
+	protected int m_steps = 1;
+	protected int m_max_steps;
 }
