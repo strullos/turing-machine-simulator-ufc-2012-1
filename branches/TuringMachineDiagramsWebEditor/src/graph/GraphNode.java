e8b38a303e557e2295a704ec0d425c93926be2ee
vitor.shade@gmail.com <vitor.shade@gmail.com@7e9d0f7c-b843-deb4-2890-b008f9f88617>
2012-10-25 03:37:44 +0000

    -Added DiagramGraphEditor and DiagramGraphDocument classes -Added GraphControlComponent to handle things related to the Graphs 	-Added method to add and to remove a node from the graph -Adde NodeControlComponent to handle node properties

--- branches/TuringMachineDiagramsWebEditor/src/graph/GraphNode.java
+++ branches/TuringMachineDiagramsWebEditor/src/graph/GraphNode.java
@@ -4,10 +4,12 @@ public class GraphNode {
 	int m_x_pos;
 	int m_y_pos;
 	String m_node_label;
-	public GraphNode(String node_label, int x_pos, int y_pos){
+	String m_module;
+	public GraphNode(String node_label, String module, int x_pos, int y_pos){
 		m_node_label = new String(node_label);
 		m_x_pos = x_pos;
 		m_y_pos = y_pos;
+		m_module = module;
 	}
 	
 	public String GetLabel()
