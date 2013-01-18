aa21c26c8df91f9cd5351066fb44c93b06a85509
vitor.shade@gmail.com <vitor.shade@gmail.com@7e9d0f7c-b843-deb4-2890-b008f9f88617>
2013-01-18 11:53:45 +0000

    -Added mouse drag -Added drag and select -Added prototype of edge style function (IT IS NOT WORKING! D:)

--- branches/TuringMachineDiagramsWebEditor/src/graph/Graph.java
+++ branches/TuringMachineDiagramsWebEditor/src/graph/Graph.java
@@ -1,35 +1,120 @@
 package graph;
 
 import java.awt.Color;
-import java.awt.event.KeyEvent;
+import java.awt.Rectangle;
+	import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.util.HashMap;
+import java.util.List;
 
 import com.mxgraph.util.mxConstants;
-import com.mxgraph.util.mxEvent;
-import com.mxgraph.util.mxEventObject;
-import com.mxgraph.util.mxEventSource;
+import com.mxgraph.util.mxPoint;
+import com.mxgraph.view.mxCellState;
+import com.mxgraph.view.mxEdgeStyle;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxStylesheet;
+import com.mxgraph.canvas.mxGraphics2DCanvas;
+import com.mxgraph.layout.mxEdgeLabelLayout;
+import com.mxgraph.layout.mxGraphLayout;
 import com.mxgraph.model.mxCell;
+import com.mxgraph.shape.mxConnectorShape;
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.swing.mxGraphOutline;
+import com.mxgraph.swing.handler.mxRubberband;
+
 
 public class Graph {
 	protected mxGraph m_graph;
 	private mxGraphComponent m_graph_component;
 	private mxGraphOutline m_graph_outline;
-	private static int m_node_width = 25;
-	private static int m_node_height = 25;
+	private mxRubberband m_graph_rubberband;
+	private static int m_node_width = 100;
+	private static int m_node_height = 100;
 	private String m_node_style;
 	private mxCell m_starting_node;
 	
+	class TestStyle implements mxEdgeStyle.mxEdgeStyleFunction{
+
+		@Override
+		public void apply(mxCellState state, mxCellState source, mxCellState target,
+				List<mxPoint> relatives, List<mxPoint> result) {
+			if(state != null)
+				System.out.println("State: " + m_graph.getLabel(state.getCell()));
+			if(source != null)
+				System.out.println("Source: " + m_graph.getLabel(source.getCell()));
+			if(target != null)
+				System.out.println("Target: " + m_graph.getLabel(target.getCell()));
+			if(relatives != null){
+				for(int i = 0; i < relatives.size(); i++){
+					System.out.println("Relatives: " + relatives.get(i).getX() + " " + relatives.get(i).getY());
+				}
+			}
+			if(result != null){
+					if(source != null){
+						//
+						System.out.println("Source: " + source.getX() + " " + source.getY());
+						System.out.println("Point count: " +  state.getAbsolutePointCount());
+						List<mxPoint> points = state.getAbsolutePoints();
+						for(int i = 0; i < points.size(); i++){
+							if(points.get(i) != null){
+								System.out.println("Point " +  i +  ": " + points.get(i).getX() + " " + points.get(i).getY());
+							}
+						}
+						if(points.get(points.size() - 1).getX() < source.getX()){
+							double x = source.getX() - state.getLength() / 2;
+							double y = source.getY();
+							result.add(new mxPoint(x, y));
+						}else{
+							double x = source.getX() + state.getLength() / 2;
+							double y = source.getY();
+							result.add(new mxPoint(x, y));
+						}
+						
+						
+					}
+					System.out.println("Edge source: " + state.getOrigin().getX());
+					if(state != null){
+						System.out.println("Width:" + state.getLength());
+						System.out.println("Origin: " + state.getOrigin().getX() + " " + state.getOrigin().getY());
+					}
+					System.out.print("Result: ");
+					for(int i = 0; i < result.size(); i++){
+						if(result.get(i) != null){
+							System.out.println("Result: " + result.get(i).getX() + " " + result.get(i).getY());
+						}
+					}
+			}
+		}
+		
+	}
 	
-	public Graph(){
+	class Test2 extends mxConnectorShape{
+		@Override
+		public void paintShape(mxGraphics2DCanvas canvas,
+                mxCellState state){
+			canvas.paintRectangle(new Rectangle(100,200), new Color(50,50,50), new Color(100,100,100));
+		}
+	}
+	
+	
+	public Graph(){		
 		m_graph = new mxGraph();		
-		m_graph_component = new mxGraphComponent(m_graph);
+		m_graph_component = new mxGraphComponent(m_graph) {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public boolean isPanningEvent(MouseEvent event){
+				if(event.getButton() == MouseEvent.BUTTON2){ //Middle Mouse Button
+					return true;
+				}
+				return false;
+			}
+		};
+		m_graph_rubberband = new mxRubberband(m_graph_component);
+		
 		m_node_style = new String("ROUNDED;fillColor=#FFFFFF");
 
 		mxStylesheet stylesheet = m_graph.getStylesheet();
@@ -37,30 +122,26 @@ public class Graph {
 		style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
 		style.put(mxConstants.STYLE_OPACITY, 100);
 		style.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-		style.put(mxConstants.STYLE_FONTSIZE, "12"); //!< Remove this later...
+		style.put(mxConstants.STYLE_FONTSIZE, "30"); //!< Remove this later...
 		style.put(mxConstants.STYLE_LABEL_POSITION, mxConstants.ALIGN_MIDDLE);
 		style.put(mxConstants.STYLE_SPACING_TOP, 5);
 		style.put(mxConstants.STYLE_STROKECOLOR, "#000000");
-		//style.put(mxConstants.STYLE_FONTSIZE, "12");
-
-
-		//         HashMap<String, Object> edge = new HashMap<String, Object>();       
-		//         edge = (HashMap<String,Object>)stylesheet.getDefaultEdgeStyle();
-		//         edge.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#FFFFFF");           
-		//     edge.put(mxConstants.STYLE_LABEL_BORDERCOLOR, "#000000");
-		//     edge.put(mxConstants.STYLE_FONTSIZE, "16");
-		//     stylesheet.setDefaultEdgeStyle(edge);
 		stylesheet.putCellStyle("ROUNDED", style);
 		
-		
 		HashMap<String, Object> edgeStyle = new HashMap<String, Object>();
-		//edgeStyle.put(mxConstants.STYLE_EDGE, mxConstants.EDGESTYLE_ORTHOGONAL);
 		edgeStyle.put(mxConstants.STYLE_SHAPE,    mxConstants.SHAPE_CONNECTOR);
 		edgeStyle.put(mxConstants.STYLE_ENDARROW, mxConstants.ARROW_CLASSIC);
 		edgeStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
 		edgeStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
+		edgeStyle.put(mxConstants.STYLE_FONTSIZE, "30");
 		edgeStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
-		stylesheet.setDefaultEdgeStyle(edgeStyle);
+		edgeStyle.put(mxConstants.STYLE_LABEL_BORDERCOLOR, "#000000");
+		edgeStyle.put( mxConstants.STYLE_EDGE, new TestStyle() );
+		stylesheet.setDefaultEdgeStyle(edgeStyle);	
+	 
+		
+		
+		
 		
 		m_graph.getModel().endUpdate();
 		m_graph.setCellsResizable(true);
@@ -68,14 +149,8 @@ public class Graph {
 		m_graph.setAllowLoops(true);      
 		//m_graph.setAutoSizeCells(true);
 
-
 		m_graph_component.setGridVisible(true);	
 		m_graph_component.setBackground(new Color(255,255,255));
-
-
-
-		m_graph_component.addMouseWheelListener(new MouseWheelTracker());	
-
 		m_graph_component.setPageVisible(true);
 		//	 PageFormat page_format = new PageFormat();
 		//	 page_format.setOrientation(PageFormat.LANDSCAPE);
@@ -85,63 +160,45 @@ public class Graph {
 
 		m_graph_outline = new mxGraphOutline(this.m_graph_component);
 
-		m_graph_component.getConnectionHandler().addListener(mxEvent.CONNECT, new mxEventSource.mxIEventListener() {
-
-			@Override
-			public void invoke(Object arg0, mxEventObject arg1) {
-				System.out.println("ADDED!");
-
-			}
-		});
-
-		m_graph_component.addListener(mxEvent.LABEL_CHANGED,new mxEventSource.mxIEventListener() {
-
-			@Override
-			public void invoke(Object arg0, mxEventObject arg1) {
-				System.out.println("RENAMED");
-
-			}
-		});
+//		m_graph_component.getConnectionHandler().addListener(mxEvent.CONNECT, new mxEventSource.mxIEventListener() {
+//
+//			@Override
+//			public void invoke(Object arg0, mxEventObject arg1) {
+//				System.out.println("ADDED!");
+//
+//			}
+//		});
+//
+//		m_graph_component.addListener(mxEvent.LABEL_CHANGED,new mxEventSource.mxIEventListener() {
+//
+//			@Override
+//			public void invoke(Object arg0, mxEventObject arg1) {
+//				System.out.println("RENAMED");
+//
+//			}
+//		});
 		
 		m_graph_component.addKeyListener(new GraphKeyListener());
+		m_graph_component.getGraphControl().addMouseListener(new GraphMouseListener());
+		m_graph_component.addMouseWheelListener(new MouseWheelTracker());	
 	}	
 
-
+	
 	public void AddNode(String label)
 	{
-		//		m_nodes.put(node.GetLabel(), node);
-		//		Object parent = m_graph.getDefaultParent();
-		//        m_graph.getModel().beginUpdate();
-		//        mxCell new_vertex = (mxCell) m_graph.insertVertex(parent, null, node.GetLabel() ,node.GetXPos(), node.GetYPos(), m_node_width, m_node_height, m_node_style);
-		//        //mxCell new_vertex = (mxCell) m_graph.insertVertex(parent, null, node.GetLabel() ,node.GetXPos(), node.GetYPos(), 500, 500, m_node_style);//!< Remove this later...
-		//        m_cell_node_map.put(new_vertex, node);
-		//        m_graph.getModel().endUpdate();  
-
 		Object parent = m_graph.getDefaultParent();
 		m_graph.getModel().beginUpdate();
-		mxCell node = (mxCell)m_graph.insertVertex(parent, null, label ,0, 0, m_node_width, m_node_height, m_node_style);
+		mxCell node = (mxCell)m_graph.insertVertex(parent, null, label ,10, 10, m_node_width, m_node_height, m_node_style);
 		m_graph.getModel().endUpdate();
 		if(m_graph.getChildCells(m_graph.getDefaultParent(), true, false).length == 1){
 			m_starting_node = node;
-			node.setStyle("ROUNDED;fillColor=#DDDDDD");
+			node.setStyle("ROUNDED;fillColor=#beffaf");
 			m_graph_component.refresh();
 		}
 	}	
 
 	public void RemoveSelectedCell()
 	{
-		////		GraphNode selected_node = GetSelectedNode();
-		////		m_nodes.remove(selected_node.GetLabel());
-		////		m_cell_node_map.remove(m_graph.getSelectionCells());
-		////		m_graph.removeCells(m_graph.getSelectionCells());
-		//		mxCell selected_vertex = (mxCell) m_graph.getSelectionCell();
-		//		System.out.println(selected_vertex.getEdgeCount());
-		//		mxCell edge = (mxCell) selected_vertex.getEdgeAt(0);
-		//		if(edge.getSource() == selected_vertex){
-		//			System.out.println("IT IS SOURCE");
-		//		}else{
-		//			System.out.println("NOT SOURCE");
-		//		}
 		m_graph.removeCells(m_graph.getSelectionCells());
 	}
 
@@ -154,11 +211,6 @@ public class Graph {
 	{
 		return m_graph_component;
 	}
-
-//	public GraphNode GetSelectedNode()
-//	{
-//		return m_cell_node_map.get(m_graph.getSelectionCell());
-//	}
 
 	public mxGraphOutline GetGraphOutline()
 	{
@@ -198,11 +250,15 @@ public class Graph {
 	{
 		if (e.getWheelRotation() < 0)
 		{
-			m_graph_component.zoomIn();			
+			if(!m_graph_component.getPanningHandler().isActive()){
+				m_graph_component.zoomIn();			
+			}
 		}
 		else
 		{
-			m_graph_component.zoomOut();			
+			if(!m_graph_component.getPanningHandler().isActive()){
+				m_graph_component.zoomOut();	
+			}
 		}		
 	}
 
@@ -211,9 +267,7 @@ public class Graph {
 
 		@Override
 		public void mouseWheelMoved(MouseWheelEvent e) {			
-			//if(e.isControlDown()){
 			Graph.this.mouseWheelMoved(e);		
-			//}				
 		}
 	};  
 	
@@ -237,5 +291,37 @@ public class Graph {
 			
 		}
 	}
+	
+	class GraphMouseListener implements MouseListener {
 
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 }
