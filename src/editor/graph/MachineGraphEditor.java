69a79ba863281e6a5b82bba9e968cfa73ad624c4
vitor.shade <vitor.shade@gmail.com>
2013-01-29 02:04:48 +0000

    -Finished methods to Import and Export a Machine Graph Diagram -Added method to set the starting node and export the machine graph -Implemented Open, Save and Save As for the Machine Graph Perspective -Added new icons -Added Machine Graph help file

--- src/editor/graph/MachineGraphEditor.java
+++ src/editor/graph/MachineGraphEditor.java
@@ -2,9 +2,11 @@ package editor.graph;
 
 import java.awt.BorderLayout;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.JFileChooser;
@@ -20,6 +22,9 @@ import editor.EditorPerspective;
 import editor.TuringMachinesEditor;
 
 import ui_utils.ClosableTabComponent;
+import ui_utils.ConfirmationFileChooser;
+import ui_utils.HelpDialog;
+import utils.StringFileReader;
 
 public class MachineGraphEditor extends EditorPerspective {
 
@@ -46,6 +51,7 @@ public class MachineGraphEditor extends EditorPerspective {
 		m_machines_tabbedPane.setSelectedComponent(machine_document);
 		m_machines_tabbedPane.setTabComponentAt(m_machines_tabbedPane.getSelectedIndex(),new ClosableTabComponent(m_machines_tabbedPane));
 		m_current_machine_graph_document = machine_document;
+		m_machines_tabbedPane.addChangeListener(new TabChangedListener());
 	}
 
 	class TabChangedListener implements ChangeListener
@@ -75,59 +81,137 @@ public class MachineGraphEditor extends EditorPerspective {
 
 	@Override
 	public void Open(String path) {
-		try {
-			m_current_machine_graph_document.m_graph.ImportGraph("v q0 150.0 390.0 true\n" + 
-					"v q2 560.0 460.0 false\n" + 
-					"v q1 560.0 290.0 false\n" + 
-					"e b;a q0 q2 393.71836996037246 494.9835715454741\n" + 
-					"e a;> q0 q1 370.0 330.0\n"	);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//		JFileChooser fc = new JFileChooser(new File("."));		
-		//		FileNameExtensionFilter filter = new FileNameExtensionFilter(
-		//		        "Machine files (.mt)", "mt");
-		//		fc.setFileFilter(filter);
-		//		fc.setAcceptAllFileFilterUsed(false);
-		//		int returnVal = fc.showOpenDialog(null);
-		//		String file_path;
-		//		if(returnVal == JFileChooser.APPROVE_OPTION){
-		//			file_path = fc.getSelectedFile().getAbsolutePath().toString();
-		//			try {
-		//				BufferedReader reader = new BufferedReader(new FileReader(file_path));
-		//				String line;
-		//				try {
-		//					String machine_text = new String("");
-		//					while( (line = reader.readLine()) != null ){
-		//						machine_text += line;		
-		//						machine_text += "\n";						
-		//					}
-		//					MachineTextDocument new_machine_document = new MachineTextDocument();
-		//					new_machine_document.SetModuleText(machine_text);
-		//					m_machines_tabbedPane.addTab(fc.getSelectedFile().getName().toString(), null, new_machine_document, null);	
-		//					m_machines_tabbedPane.setSelectedComponent(new_machine_document);
-		//					m_machines_tabbedPane.setTabComponentAt(m_machines_tabbedPane.getSelectedIndex(),new ClosableTabComponent(m_machines_tabbedPane));
-		//					TuringMachinesEditor.SetStatusMessage("Machine file loaded successfully.\n");
-		//				} catch (IOException e) {
-		//					e.printStackTrace();
-		//				}
-		//			} catch (FileNotFoundException e) {
-		//				e.printStackTrace();
-		//			}
-		//		}		
+		JFileChooser fc = new JFileChooser(new File("."));		
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				"Machine Graph files (.gmt)", "gmt");
+		fc.setFileFilter(filter);
+		fc.setAcceptAllFileFilterUsed(false);
+		int returnVal = fc.showOpenDialog(null);
+		String file_path;
+		if(returnVal == JFileChooser.APPROVE_OPTION){
+			file_path = fc.getSelectedFile().getAbsolutePath().toString();
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(file_path));
+				String line;
+				try {
+					String machine_text = new String("");
+					while( (line = reader.readLine()) != null ){
+						machine_text += line;		
+						machine_text += "\n";						
+					}
+					MachineGraphDocument new_machine_document = new MachineGraphDocument();
+					new_machine_document.m_graph.ImportGraph(machine_text);					
+					m_machines_tabbedPane.addTab(fc.getSelectedFile().getName().toString(), null, new_machine_document, null);	
+					m_machines_tabbedPane.setSelectedComponent(new_machine_document);
+					m_machines_tabbedPane.setTabComponentAt(m_machines_tabbedPane.getSelectedIndex(),new ClosableTabComponent(m_machines_tabbedPane));
+					TuringMachinesEditor.SetStatusMessage("Machine file loaded successfully.\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}		
 	}
 
 	@Override
 	public void Save() {
-		// TODO Auto-generated method stub
-
+		if(m_current_machine_graph_document == null){
+			return;
+		}
+		String machine_graph_text = m_current_machine_graph_document.GetMachineText();
+		if(machine_graph_text.isEmpty()){
+			TuringMachinesEditor.SetStatusMessage("Empty machine.");
+		}else{
+			String machine_path = m_current_machine_graph_document.GetMachineGraphDocumentPath();
+			String machine_name = "";
+			if(machine_path.isEmpty()){
+				ConfirmationFileChooser fc = new ConfirmationFileChooser(new File("."));
+				FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				        "Machine Graph files (.gmt)", "gmt");
+				fc.setFileFilter(filter);
+				fc.setAcceptAllFileFilterUsed(false);
+				int returnVal = fc.showSaveDialog(null);
+				if(returnVal == JFileChooser.APPROVE_OPTION){	
+					machine_path = fc.getSelectedFile().getAbsolutePath().toString();
+					if(!machine_path.endsWith(".gmt")){
+						machine_path = machine_path + ".gmt";
+					}
+					machine_name = fc.getSelectedFile().getName();
+					if(!machine_name.endsWith(".gmt")){
+						machine_name = machine_name + ".gmt";
+					}
+					m_current_machine_graph_document.SetMachineDocumentPath(machine_path);
+				}else{
+					return;
+				}
+			}			
+			FileWriter fstream; 
+			try {
+				fstream = new FileWriter(machine_path);
+				BufferedWriter out = new BufferedWriter(fstream);
+				out.write(m_current_machine_graph_document.m_graph.ExportGraph());
+				out.close();
+				TuringMachinesEditor.SetStatusMessage("Machine Graph file " + machine_path + " saved succesfully.\n");
+				if(!machine_name.isEmpty()){ //If machine name is empty the file was already saved so we do not need to save it again
+					m_machines_tabbedPane.setTitleAt(m_machines_tabbedPane.getSelectedIndex(), machine_name);
+				}				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}						
+		}
 	}
 
 	@Override
 	public void SaveAs() {
-		// TODO Auto-generated method stub
-
+		if(m_current_machine_graph_document == null){
+			return;
+		}
+		String machine_graph_text = m_current_machine_graph_document.GetMachineText();
+		if(machine_graph_text.isEmpty()){
+			TuringMachinesEditor.SetStatusMessage("Empty machine.");
+		}else{
+			String machine_path = m_current_machine_graph_document.GetMachineGraphDocumentPath();
+			String machine_name = "";
+			if(machine_path.isEmpty()){
+				ConfirmationFileChooser fc = new ConfirmationFileChooser(new File("."));
+				FileNameExtensionFilter filter = new FileNameExtensionFilter(
+				        "Machine Graph files (.gmt)", "gmt");
+				fc.setFileFilter(filter);
+				fc.setAcceptAllFileFilterUsed(false);
+				int returnVal = fc.showSaveDialog(null);
+				if(returnVal == JFileChooser.APPROVE_OPTION){	
+					machine_path = fc.getSelectedFile().getAbsolutePath().toString();
+					if(!machine_path.endsWith(".gmt")){
+						machine_path = machine_path + ".gmt";
+					}
+					machine_name = fc.getSelectedFile().getName();
+					if(!machine_name.endsWith(".gmt")){
+						machine_name = machine_name + ".gmt";
+					}
+					m_current_machine_graph_document.SetMachineDocumentPath(machine_path);
+				}else{
+					return;
+				}
+			}			
+			FileWriter fstream; 
+			try {
+				String machine_content = m_current_machine_graph_document.m_graph.ExportGraph();
+				fstream = new FileWriter(machine_path);
+				BufferedWriter out = new BufferedWriter(fstream);
+				out.write(machine_content);
+				out.close();
+				TuringMachinesEditor.SetStatusMessage("Machine Graph file " + machine_path + " saved succesfully.\n");
+				New();
+				if(!machine_name.isEmpty()){ //If machine name is empty the file was already saved so we do not need to save it again
+					m_machines_tabbedPane.setTitleAt(m_machines_tabbedPane.getSelectedIndex(), machine_name);
+				}		
+				m_current_machine_graph_document.SetMachineDocumentPath(machine_path);
+				m_current_machine_graph_document.m_graph.ImportGraph(machine_content);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}						
+		}
 	}
 
 	@Override
@@ -159,18 +243,18 @@ public class MachineGraphEditor extends EditorPerspective {
 				e.printStackTrace();
 			}
 		}
+		TuringMachinesEditor.SetStatusMessage("Execution finished.\n");
 	}
 
 	@Override
 	public void Help() {
-		// TODO Auto-generated method stub
-
+		HelpDialog help_dialog = new HelpDialog();
+		help_dialog.SetHelpContent(StringFileReader.ReadFile(getClass().getResourceAsStream("/help/machine_graph_help.txt")));
+		help_dialog.setVisible(true);	
 	}
 
 	@Override
 	public void Examples() {
 		// TODO Auto-generated method stub
-
 	}
-
 }
